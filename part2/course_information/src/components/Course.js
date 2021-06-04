10e6e33d437940bfa2150816194f9b665d7615ed
Benjamintdk <bc.chew.hong.ee@gmail.com>
2021-06-05 06:14:54 +0800

    finished exercises 2.1 to 2.5 for part2

---
+++ part2/course_information/src/components/Course.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Header = ({ name }) => {
+    return (
+      <div>
+        <h1>{name}</h1>
+      </div>
+    )
+  }
+  
+  const Part = ({ name, exercise }) => {
+    return (
+      <div>
+        <p>
+          {name} {exercise}
+        </p>
+      </div>
+    )
+  }
+  
+  const Content = ({ parts }) => {
+    return (
+      <div>
+        {parts.map(part =>
+           <Part key={part.id} name={part.name} exercise={part.exercises} />
+        )}
+      </div>
+    )
+  }
+  
+  const Total = ({ parts }) => {
+    return (
+      <div>
+        total of {parts.reduce((a,b) =>
+        a+b.exercises, 0)}
+      </div>
+    )
+  }
+  
+  const Course = ({ course }) => {
+    const { name, parts } = course
+    return (
+      <div>
+        <Header name={name} />
+        <Content parts={parts} />
+        <b><Total parts={parts} /></b>
+      </div>
+    )
+  }
+
+  export default Course
