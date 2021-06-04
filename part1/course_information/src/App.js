2ab80c45dd7a903f2bc0ecc92c2048ed49420ffb
Benjamintdk <bc.chew.hong.ee@gmail.com>
2021-06-05 06:52:20 +0800

    created phonebook app

--- part1/course_information/src/App.js
+++ part1/course_information/src/App.js
@@ -1,37 +1,37 @@
 import React from 'react'
 
-const Header = (course) => {
+const Header = (props) => {
   return (
     <div>
-      <h1>{course.course.name}</h1>
+      <h1>{props.course.name}</h1>
     </div>
   )
 }
 
-const Part = (course) => {
+const Part = (props) => {
   return (
     <div>
       <p>
-        {course.part} {course.exercise}
+        {props.part} {props.exercise}
       </p>
     </div>
   )
 }
 
-const Content = (courses) => {
+const Content = (props) => {
   return (
     <div>
-      <Part part={courses.course.parts[0].name} exercise={courses.course.parts[0].exercises} />
-      <Part part={courses.course.parts[1].name} exercise={courses.course.parts[1].exercises} />
-      <Part part={courses.course.parts[2].name} exercise={courses.course.parts[2].exercises} />
+      <Part part={props.course.parts[0].name} exercise={props.course.parts[0].exercises} />
+      <Part part={props.course.parts[1].name} exercise={props.course.parts[1].exercises} />
+      <Part part={props.course.parts[2].name} exercise={props.course.parts[2].exercises} />
     </div>
   )
 }
 
-const Total = (exercises) => {
+const Total = (props) => {
   return (
     <div>
-      <p>Number of exercises {exercises.course.parts[0].exercises + exercises.course.parts[1].exercises + exercises.course.parts[2].exercises}</p>
+      <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
     </div>
   )
 }
